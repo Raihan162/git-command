9fdc2db467f31ecb7b7e300befa400eceea153ae
Raihan162 <muhammadraihanarrafi@gmail.com>
2024-01-15 20:36:10 +0700

    Update Countdown

--- week2/day6/countdown/src/pages/Home/components/ShowCounter/index.jsx
+++ week2/day6/countdown/src/pages/Home/components/ShowCounter/index.jsx
@@ -5,39 +5,54 @@ import classes from './style.module.scss'
 export default function ShowCounter({ days, hours, minutes, seconds }) {
     return (
         <div className={classes.showCounter}>
-            <div className={classes.days}>
-                <div className={classes.daysTime}>
-                    {days < 10 && days >= 0 ? `0${days}` : days >= 10 ? days : '00'}
+            <div className={classes.containerTime}>
+                <div className={classes.card}>
+                    <div className={classes.time}>
+                        {days < 10 && days >= 0 ? `0${days}` : days >= 10 ? days : '00'}
+                    </div>
+                    <div className={classes.top}></div>
+                    <div className={classes.bottom}></div>
                 </div>
                 <p>
                     DAYS
                 </p>
             </div>
-            <div className={classes.hours}>
-                <div className={classes.hoursTime}>
-                    {hours < 10 && hours >= 0 ? `0${hours}` : hours >= 10 ? hours : '00'}
-                    {/* {hours < 10 ? `0${hours}` : hours || '00'} */}
+            <div className={classes.containerTime}>
+                <div className={classes.card}>
+                    <div className={classes.time}>
+                        {hours < 10 && hours >= 0 ? `0${hours}` : hours >= 10 ? hours : '00'}
+                    </div>
+                    <div className={classes.top}></div>
+                    <div className={classes.bottom}></div>
                 </div>
                 <p>
                     HOURS
                 </p>
             </div>
-            <div className={classes.minutes}>
-                <div className={classes.minutesTime}>
-                    {minutes < 10 && minutes >= 0 ? `0${minutes}` : minutes >= 10 ? minutes : '00'}
+            <div className={classes.containerTime}>
+                <div className={classes.card}>
+                    <div className={classes.time}>
+                        {minutes < 10 && minutes >= 0 ? `0${minutes}` : minutes >= 10 ? minutes : '00'}
+                    </div>
+                    <div className={classes.top}></div>
+                    <div className={classes.bottom}></div>
                 </div>
                 <p>
                     MINUTES
                 </p>
             </div>
-            <div className={classes.seconds}>
-                <div className={classes.secondsTime}>
-                    {seconds < 10 && seconds >= 0 ? `0${seconds}` : seconds >= 10 ? seconds : '00'}
+            <div className={classes.containerTime}>
+                <div className={classes.card}>
+                    <div className={classes.time}>
+                        {seconds < 10 && seconds >= 0 ? `0${seconds}` : seconds >= 10 ? seconds : '00'}
+                    </div>
+                    <div className={classes.top}></div>
+                    <div className={classes.bottom}></div>
                 </div>
                 <p>
                     SECONDS
                 </p>
             </div>
-        </div>
+        </div >
     )
 }
